099e52b45ed6be9d51dd242cf1e997372c7ef0a6
Hollow <hollow1337@gmail.com>
2023-03-07 23:00:34 +0300

    first commit

---
+++ home.Work1/Program.cs
@@ -0,0 +1,99 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+*/
+Console.WriteLine("Введи 2 числа и мы тебе скажем какое из них больше");
+
+int max = 0;
+int min = 0;
+
+Console.Write("Number1: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Number3: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+
+if(Number1 > Number2)
+{
+    max = Number1;
+    min = Number2;
+}
+else
+{
+    max = Number2;
+    min = Number1;
+}
+Console.WriteLine("max = " + max + " и " + "min = " + min);
+
+
+/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел. 
+*/
+
+Console.WriteLine("Введи 3 числа и мы тебе скажем какое из них больше");
+
+int max = 0;
+
+Console.Write("Input Number1: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input Number2: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input Number3: ");
+int Number3 = Convert.ToInt32(Console.ReadLine());
+
+if(Number1 > max)
+
+    max = Number1;
+
+if(Number2 > max)
+    max = Number2;
+    
+if(Number3 > max)
+        
+    max = Number3;
+        
+
+Console.WriteLine("max = " + max);
+
+
+
+/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным.
+*/
+
+Console.WriteLine("Введи число и мы узнаем четное ли оно");
+
+Console.Write("Input Number1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = number % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine("Yes!");
+}
+else
+{
+    Console.WriteLine("No!");
+}
+
+/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+*/
+
+Console.WriteLine("Введи число и мы увидим все четные до него");
+
+Console.Write("Input num1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if(number > 1)
+{
+while(evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
---
+++ seminar1/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+Console.WriteLine("Good Evening" );
+// int integer = 34;
+// double doublenum = 2.11111;
+
+// string world = "Hello";
+// char symbol = 'f';
+
+// bool check = true;
+
+//int num1 = 5;
+//int num2 = 10;
+
+//Console.WriteLine("My number is " + num + " and " + num1 +  " and is good ");
+//Console.WriteLine($"My number are  {num}  and  {num1}  and is good");
+//Console.Write("input: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+Console.Write("Введите число 1 ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2 ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = num2 * num2;
+
+if(num1 == quad2)
+{
+    Console.WriteLine("Yes!");
+}
+else
+{
+    Console.WriteLine("No!");
+}
+
+Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Input a positive number ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = -number;
+
+while (current <= number)
+{
+    Console.Write(current + " ");
+    current++;
+} 
+
+32141 / 10 = 3214
+32141 / 100 = 321
+32141 / 1000 = 32
+32141 / 100000 = 3
+
+32143 % 10 = 3
+32143 % 100 = 43
+32143 % 1000 = 143
+32143 % 10000 = 2143
+
+32141 / 100 % 100 
+234522 / 10 % 100 
+31235 / 100 % 100 
+*/
