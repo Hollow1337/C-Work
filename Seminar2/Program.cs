e29739de38eb113a8219611067b419f8d65e43a3
Hollow <hollow1337@gmail.com>
2023-03-12 19:38:51 +0300

    ДЗ и другое

---
+++ Seminar2/Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+int CutNumber(int num)
+{
+    int hundreds = num / 100;
+    int units = num % 10;
+
+    int result = hundreds * 10 + units;
+    return result;
+
+}
+
+
+int randNumber = new Random().Next(100, 1000);
+
+int newNumber = CutNumber(randNumber);
+
+Console.WriteLine($"New version of {randNumber} is {newNumber}");
+*/
+
+//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+/*
+int MaxNumber(int num);
+{
+    int decades = num / 10;
+    int units = MaxNum % 10;
+    int max = decades;
+
+    if (units > decades)
+
+    {
+        max = units;
+    }
+    return max;
+
+}
+
+int number = new Random().Next(10, 100);
+
+int maxNum = MaxNumber(number);
+
+Console.WriteLine($"Максимальная цифра {number} - {maxNum}. ");
+
+*/
+
+//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно a и b.
+/*
+bool Multiplisity(int userNum, int a, int b);
+{
+    if (userNum % a == 0 && userNum % b == 0)
+        return true;
+    else
+         return false;
+}
+
+
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число a");
+
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b");
+
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число проверки");
+
+bool resMult = Multiplisity(number, a, b);
+
+Console.WriteLine($"Проверка на кратность - {resMult}");
+*/
---
+++ home.Work2/Program.cs
@@ -0,0 +1,62 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+Console.Clear();
+
+Console.WriteLine("Hello leather bas**** ");
+
+Console.Write("Enter the figure of the day of the week : ");
+
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("(Weekend");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("And what did you write? There are only 7 of them!");
+  }
+  else Console.WriteLine("Not weekend! ");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
+*/
+
+
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+/*
+Console.Clear();
+
+Console.WriteLine("Hello leather bas**** ");
+
+Console.Write("Enter number: ");
+
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+
+string anyNumberText = Convert.ToString(anyNumber);
+
+if (anyNumberText.Length > 2){
+
+  Console.WriteLine("Three num -> " + anyNumberText[2]);
+
+}
+
+else {
+
+  Console.WriteLine(" There is no third digit ");
+
+}
+*/
+//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+Console.Clear();
+
+Console.WriteLine("Hello leather bas**** ");
+
+Console.Write("Enter a three-digit number ");
+
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+string stringNumber = Convert.ToString(threeDigitNumber);
+
+Console.WriteLine($"the second digit of this number {stringNumber[1]}");
